93faba02d23196c5f3c33ed4add11b1c5957e72d
tots_am <tomasbhmg@gmail.com>
2025-05-22 12:14:20 -0300

    feat: adicionei um main com as funções básicas para desenhar uma janela

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,18 @@
-// escrevi um código
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <unistd.h>
+#include <iostream>
+
+const float FPS = 30;              // Define FPS do Jogo
+const int SCREEN_WIDTH = 800;      // Define o comprimento da janela do Jogo
+const int SCREEN_LENGHT = 600;     // Define a largura da janela do jogo
+
+int main(int argc, char **argv){
+    ALLEGRO_DISPLAY *display = NULL;
+
+    al_init();
+    al_init_primitives_addon();
+    display = al_create_display(SCREEN_WIDTH, SCREEN_LENGHT);
+
+    return 0;
+}
