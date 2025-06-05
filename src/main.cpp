7c86c17ed1a7710e7534e188e84103daddf92b90
tots_am <tomasbhmg@gmail.com>
2025-06-05 15:39:06 -0300

    feat: iniciando e instanciando objetos e assets

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@
 #include <allegro5/allegro_image.h>
 #include <iostream>
 
-const float FPS = 30;              // Define FPS do Jogo
+const float FPS = 30;               // Define FPS do Jogo
 const int SCREEN_WIDTH = 800;      // Define o comprimento da janela do Jogo
-const int SCREEN_LENGHT = 600;     // Define a largura da janela do jogo
+const int SCREEN_HEIGHT = 600;      // Define a largura da janela do jogo
 
 // Define as fontes
 const std::string ARIAL_FONT_PATH = "./assets/fonts/arial.ttf";
@@ -18,7 +18,7 @@ const std::string BACKGROUND_IMG_PATH = "./assets/sprites/background-800-600.png
 const std::string BASE_IMG_PATH = "./assets/sprites/base-800-50.png";
 const std::string BIRD_IMG_PATH = "./assets/sprites/yellowbird-midflap.png";
 
-// Define os estados que o Jogo pode Estar
+// Define as telas que o Jogo pode Estar
 enum gameState{
     inStartMenu,
     inGame,
@@ -32,10 +32,88 @@ int main(){
     ALLEGRO_EVENT_QUEUE *eventQueue = NULL;
     ALLEGRO_TIMER *timer = NULL;
 
+    //Inicializando objetos Allegro e Carregando Assets
+    if (!al_init()) {
+        std::cout << "Falha ao iniciar allegro" << std::endl;
+        return -1;
+    }
 
-    al_init();
-    al_init_primitives_addon();
-    display = al_create_display(SCREEN_WIDTH, SCREEN_LENGHT);
+    // Iniciando Fontes
+    if(!al_init_font_addon()){
+        std::cout << "Falha ao iniciar pacote de fontes Allegro" << std::endl;
+        return -1;
+    }
+
+    if(!al_init_ttf_addon()){
+        std::cout << "Falha ao iniciar pacote de fontes ttf Allegro" << std::endl;
+        return -1;
+    }
+
+    // Seção para carregar fontes
+    ALLEGRO_FONT *font_arial = al_load_font(ARIAL_FONT_PATH.c_str(), FONT_SIZE, 0);
+    if(font_arial == nullptr){
+        std::cout << "Falha ao iniciar fonte" << std::endl;
+        return -1;
+    }
+
+    // Iniciando imagens
+    if(!al_init_image_addon()){
+        std::cout << "Falha ao iniciar pacote de imagens do Allegro" << std::endl;
+        return -1;
+    }
+    
+    // Seção para carregar imagens
+    ALLEGRO_BITMAP *background = al_load_bitmap(BACKGROUND_IMG_PATH.c_str());
+    if(background == nullptr){
+        std::cout << "Falha ao carregar background" << std::endl;
+        return -1;
+    }
+
+    ALLEGRO_BITMAP *base = al_load_bitmap(BASE_IMG_PATH.c_str());
+    if(base == nullptr){
+        std::cout << "Falha ao carregar base" << std::endl;
+        return -1;
+    }
+
+    ALLEGRO_BITMAP *bird = al_load_bitmap(BIRD_IMG_PATH.c_str());
+    if(bird == nullptr){
+        std::cout << "Falha ao carregar passaro" << std::endl;
+        return -1;
+    }
+
+    // Iniciando Primitivas
+    if (!al_init_primitives_addon()){
+        std::cout << "Falha ao iniciar pacote de primitivas do Allegro" << std::endl;
+        return -1;
+    }
+
+    // Instanciando fila de eventos
+    eventQueue= al_create_event_queue();
+    if (!eventQueue) {
+        std::cout << "Falha ao iniciar fila de eventos" << std::endl;
+        return -1;
+    }
+
+    // Iniciando teclado
+    if (!al_install_keyboard()) {
+        std::cout << "Falha ao iniciar teclado" << std::endl;
+        return -1;
+    }
+
+    // Instanciando display
+    display = al_create_display(SCREEN_WIDTH, SCREEN_HEIGHT);
+    if(!display){
+        std::cout << "Falha ao criar display" << std::endl;
+        return -1;
+    }
+
+    // Instanciando timer
+    timer = al_create_timer(1.0/FPS);
+    if(!timer){
+        std::cout << "Falha ao Iniciar Timer" << std::endl;
+        al_destroy_display(display);
+        return -1;
+    }
 
     return 0;
 }
