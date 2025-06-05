b12f18b5366a6d4c7be389f5f7604bebc6d76e2e
tots_am <tomasbhmg@gmail.com>
2025-06-05 15:31:38 -0300

    feat: definicao de imagens e fontes

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,16 @@ const float FPS = 30;              // Define FPS do Jogo
 const int SCREEN_WIDTH = 800;      // Define o comprimento da janela do Jogo
 const int SCREEN_LENGHT = 600;     // Define a largura da janela do jogo
 
-int main(int argc, char **argv){
+// Define as fontes
+const std::string ARIAL_FONT_PATH = "./assets/fonts/arial.ttf";
+const int FONT_SIZE = 32;
+
+//Define as imagens
+const std::string BACKGROUND_IMG_PATH = "./assets/sprites/background-800-600.png";
+const std::string BASE_IMG_PATH = "./assets/sprites/base-800-50.png";
+const std::string BIRD_IMG_PATH = "./assets/sprites/yellowbird-midflap.png";
+
+int main(){
     ALLEGRO_DISPLAY *display = NULL;
 
     al_init();
