00f11ee4803f8e0cd53a0dae44dcc062843f5911
Aloquis <yuri.viana.aguiar@gmail.com>
2025-06-17 21:06:31 -0300

    doc(bird): Documentação da Classe no estilo Doxygen adicionada

--- src/birdClass.cpp
+++ src/birdClass.cpp
@@ -7,18 +7,20 @@ Bird::Bird(float pos_x_inicial, float pos_y_inicial, const std::string &bird_img
         vel_y(0.0f),
         pos_x(pos_x_inicial), pos_y(pos_y_inicial),
         BIRD_IMG_PATH(bird_img_path), 
-        bird(al_load_bitmap(bird_img_path.c_str())),
-        largura_obj(al_get_bitmap_width(bird)), altura_obj(al_get_bitmap_height(bird))
+        bird(al_load_bitmap(bird_img_path.c_str()))
     {
         if (bird == nullptr) {
             throw std::runtime_error("Erro ao inicializar o pássaro: " + bird_img_path);
         }
+        
+        this->largura_obj = al_get_bitmap_width(bird);
+        this->altura_obj = al_get_bitmap_height(bird);
     }   
 
 Bird::Bird() :
     vel_y(0.0f),
     pos_x(0.0f), pos_y(0.0f),
-    BIRD_IMG_PATH(nullptr),
+    BIRD_IMG_PATH(""),
     bird(nullptr),
     largura_obj(0.0f), altura_obj(0.0f)
     {}    
@@ -33,13 +35,20 @@ void Bird::forced_draw(float pos_x, float pos_y) {
     al_draw_bitmap(bird, pos_x, pos_y, 0);
 }
 
-void Bird::forced_update(float pos_x, float pos_y) {
+void Bird::set_position(float pos_x, float pos_y) {
     this-> pos_x = pos_x;
     this-> pos_y = pos_y;
 }
 
 void Bird::draw() {
-    al_draw_bitmap(bird, this-> pos_x, this-> pos_y, 0);
+    if (!bird) {
+        return;
+    }
+
+    float draw_x = this->pos_x - (this->largura_obj / 2.0f);
+    float draw_y =  this->pos_y - (this->altura_obj / 2.0f);
+
+    al_draw_bitmap(bird, draw_x, draw_y, 0);
 
 }
 
