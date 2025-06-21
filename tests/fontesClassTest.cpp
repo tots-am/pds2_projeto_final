19259f3c8e76d0ff5517a9c8ac44fe9e16cfe307
Isabela Lima <belaglimacsc@gmail.com>
2025-06-21 15:40:28 -0300

    feat/fontes doctest completo

--- tests/fontesClassTest.cpp
+++ tests/fontesClassTest.cpp
@@ -8,7 +8,7 @@
 bool iniciar_al(){
     return al_init() && al_init_font_addon() && al_init_ttf_addon();
 }
-TEST_CASE("Teste de inicialização da fontesClass"){
+TEST_CASE("Teste de inicialização da fontesClass .ttf"){
     CHECK(iniciar_al());
 
     const char* caminho = "assets/fonts/arial.ttf";
@@ -17,3 +17,21 @@ TEST_CASE("Teste de inicialização da fontesClass"){
     fontesClass fonte(caminho, tam);
     CHECK(fonte.getfonte() != nullptr);
 }
+TEST_CASE("Carregamento de fonte bitmap .fnt") {
+    CHECK(iniciar_al());
+    fontesClass fonte;
+    CHECK(fonte.getfonte() != nullptr);
+}
+TEST_CASE("Teste de escrever, sem recurso visual"){
+    CHECK(iniciar_al());
+
+    al_create_display(800,600);
+
+    fontesClass fonte("assets/fonts/arial.ttf", 24);
+    ALLEGRO_COLOR cor = al_map_rgb(255, 255, 255);
+
+    CHECK_NOTHROW(fonte.escrever("Texto de Teste", 100, 100, cor, ALLEGRO_ALIGN_CENTER));
+
+    al_destroy_display(al_get_current_display());
+}
+
