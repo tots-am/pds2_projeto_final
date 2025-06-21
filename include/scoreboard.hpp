a27196980e8ae98296f04024ed49562426f22a63
tots-am <tomasbhmg@gmail.com>
2025-06-20 21:48:16 -0300

    feat: modified scoreboard hpp to accomodate

--- include/scoreboard.hpp
+++ include/scoreboard.hpp
@@ -1,29 +1,40 @@
 #ifndef SCOREBOARD_H
 #define SCOREBOARD_H
 
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_primitives.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include "fontesClass.hpp"
+#include "db.hpp"
 
 using namespace std;
 
 class Scoreboard{
     
     int num_players;
-    vector <string> player_names;
-    bool isDbEmpty();
+    vector <string> linhas;
+    ALLEGRO_FONT *fonte;
+    DB database;
 
     public:
-    Scoreboard();
+
+    Scoreboard(ALLEGRO_FONT *fonte);
+
+    bool isDbEmpty();
 
     void drawScoreboard();
 
     void exibeInfos();
 
-    void updatePlayerInfo();
+    void addPlayerInfo(string player);
 
-    bool PlayerExists();
+    bool PlayerExists(string player);
+
+    void updateNumPlayers();
 };
 
 #endif
