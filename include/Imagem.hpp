5663b4c80d3006326948ef48331ec9a61799830c
tots-am <tomasbhmg@gmail.com>
2025-06-22 14:23:00 -0300

    feat: remake classe de imagens

--- include/Imagem.hpp
+++ include/Imagem.hpp
@@ -8,20 +8,16 @@
 
 using namespace std;
 
-class Imagem{
-    private:
-      const std::string BACKGROUND_IMG_PATH = "./assets/sprites/background-800-600.png";
-      const std::string BASE_IMG_PATH = "./assets/sprites/base-800-50.png";
-      const std::string BIRD_IMG_PATH = "./assets/sprites/yellowbird-midflap.png";
-      ALLEGRO_BITMAP * base;
-      ALLEGRO_BITMAP * background;
-      ALLEGRO_BITMAP * bird;
-
+class Imagem{  
     public:
-      int IniciaImagens();
-      int CarregaImagens();
-      void DesenhaBackground();
-      void DesenhaBase();
-      void DesenhaBird(float posY);
-      void DestroiImagens();
+      ALLEGRO_BITMAP * imagem;
+      const string path;
+      int largura;
+      int altura;
+
+      Imagem();
+      Imagem(const string img_path);
+      ~Imagem();
+      void exibir(float pos_x, float pos_y);
+      bool isLoaded();
 };
