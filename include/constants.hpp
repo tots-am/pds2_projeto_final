b160c829f73a301eedefd0eb2261cad252fc8ec7
Isabela Lima <belaglimacsc@gmail.com>
2025-06-22 14:12:06 -0300

    feat/mudando tammanho da fonte flappy

--- include/constants.hpp
+++ include/constants.hpp
@@ -15,7 +15,7 @@ const std::string ARIAL_FONT_PATH = "./assets/fonts/arial.ttf";
 constexpr int ARIAL_FONT_SIZE = 32;
 
 const std::string FLAPPY_FONT_PATH = "assets/fonts/flappy-font.ttf";
-constexpr int FLAPPY_FONT_SIZE =32;
+constexpr int FLAPPY_FONT_SIZE = 40;
 
 constexpr float FPS = 30;              // Define FPS do Jogo
 constexpr int SCREEN_WIDTH = 800;      // Define o comprimento da janela do Jogo
