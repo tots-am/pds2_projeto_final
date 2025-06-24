302aa1f47d74ebdab21282ea2860d7049bf52c62
Isabela Lima <belaglimacsc@gmail.com>
2025-06-24 09:26:28 -0300

    feat/função para lidar com arquivo no dbTest

--- tests/dbTest.cpp
+++ tests/dbTest.cpp
@@ -1,4 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "db.hpp"
-#include <cstdio> 
+#include <cstdio>
+
+const std::string arquivo_teste = "test_db.txt"; //declaração do arquivo a ser manuseado
+
+void limparArquivo() {
+    std::ofstream file(arquivo_teste, std::ios::trunc); //garante que arquivo está limpo
+    file.close();
+}
