71b34268c4ffc4e3ebbedf57694b235edd4af532
Isabela Lima <belaglimacsc@gmail.com>
2025-06-24 09:43:51 -0300

    feat/bibliotecas do teste scoreboard

--- tests/scoreboardTest.cpp
+++ tests/scoreboardTest.cpp
@@ -1,1 +1,6 @@
 //doctest para updateNumPlayer e updatePLayer info, interfaces testaveis sem aspecto grafico
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "scoreboard.hpp"
+#include <cstdio>
+
